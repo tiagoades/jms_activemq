d1e8ebf6518e2139d0b90cfa26088beb6b2d5cca
tiagoades <tiago.almeida.s@gmail.com>
2020-04-22 18:05:54 -0300

    Primeira e segunda aula do curso de JMS e ActiveMQ da Alura

---
+++ jms/src/br/com/caelum/jms/TesteConsumidor.java
@@ -0,0 +1,51 @@
+package br.com.caelum.jms;
+
+import java.util.Scanner;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+public class TesteConsumidor {
+
+	@SuppressWarnings("resource")
+	public static void main(String[] args) throws NamingException, JMSException {
+		
+		InitialContext context = new InitialContext();
+		
+		ConnectionFactory factory = (ConnectionFactory) context.lookup("ConnectionFactory");
+		
+		Connection connection = factory.createConnection();
+		
+		connection.start();
+
+		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+
+		Destination fila = (Destination) context.lookup("finaceiro");
+
+		MessageConsumer consumer = session.createConsumer(fila);
+				
+		//Message message = consumer.receive();
+		
+		TextMessage txt = (TextMessage) consumer.receive();
+		
+		//System.out.println("Recebendo a mensagem: "+message);		
+		
+		System.out.println("Recebendo a mensagem: "+txt.getText());		
+		
+		new Scanner(System.in).nextLine();
+
+		session.close();
+		connection.close();
+		context.close();
+		
+	}
+
+}
